7b267983cff4e7f68d547d36a1bd1ea62b041136
Hankin <hankin_h@126.com>
2023-04-12 15:28:10 +0800

    增加附属

--- src/CanComment.php
+++ src/CanComment.php
@@ -7,12 +7,14 @@ use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 trait CanComment
 {
-    public function comment(Commentable $commentable, $content, $stars=null, $pics=[])
+    public function comment(Commentable $commentable, $dependable, $content, $stars=null, $pics=[])
     {
         $commentModel = config('comment.model');
         $comment = new $commentModel([
             'content'       =>  $content,
             'pics'          =>  json_encode($pics),
+            'dependable_id' =>  $dependable->id,
+            'dependable_type' =>  $dependable::class,
             'stars'         => $stars,
             'commented_id'   => (string)$this->getAttribute($this->primaryKey),
             'commented_type' => get_class(),
